c8a5c48e60795a75cad26fe0740d239954a1b5cc
Jarius Jenkins <jarius.jenkins@gmail.com>
2021-04-18 20:49:26 +0900

    Allow internal corporate URL overrides.
    Internal corporate implementation rather than the general Microsoft one.

--- lib/ueberauth/strategy/microsoft/oauth.ex
+++ lib/ueberauth/strategy/microsoft/oauth.ex
@@ -48,8 +48,8 @@ defmodule Ueberauth.Strategy.Microsoft.OAuth do
     [
       strategy: __MODULE__,
       site: "https://graph.microsoft.com",
-      authorize_url: "https://login.microsoftonline.com/#{tenant_id}/oauth2/v2.0/authorize",
-      token_url: "https://login.microsoftonline.com/#{tenant_id}/oauth2/v2.0/token",
+      authorize_url: config[:authorize_url] || "https://login.microsoftonline.com/#{tenant_id}/oauth2/v2.0/authorize",
+      token_url: config[:token_url] || "https://login.microsoftonline.com/#{tenant_id}/oauth2/v2.0/token",
       request_opts: [ssl_options: [versions: [:"tlsv1.2"]]]
     ]
   end
